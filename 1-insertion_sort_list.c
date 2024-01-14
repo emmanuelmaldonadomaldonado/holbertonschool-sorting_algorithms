5767c7d90f91a2d00886282ed7326528a7dbf26f
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-14 16:43:55 -0400

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,86 +1,43 @@
-/*#include "sort.h"
+#include "sort.h"
 #include <stdio.h>
 
-**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- * @list: Pointer to the head of the linked list
- *
-void insertion_sort_list(listint_t **list)
-{
-    if (!list || !(*list) || !(*list)->next)
-        return;
-
-    listint_t *curr, *prev, *temp;
-
-    curr = (*list)->next;
-
-    while (curr)
-    {
-        temp = curr;
-
-        while (temp->prev && temp->n < temp->prev->n)
-        {
-            prev = temp->prev;
-
-            if (temp->next)
-                temp->next->prev = prev;
-            prev->next = temp->next;
-            temp->next = prev;
-            temp->prev = prev->prev;
-            prev->prev = temp;
-
-            if (temp->prev)
-                temp->prev->next = temp;
-            else
-                *list = temp;
-
-            print_list(*list);
-        }
-
-        curr = curr->next;
-    }
-}*/
-
-#include "sort.h"
-
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * @list: double pointer to the head of the list
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                       using the Insertion sort algorithm
+ * @list: Pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
+    if (!list || !*list || !(*list)->next)
         return;
 
     listint_t *curr, *prev, *temp;
 
     curr = (*list)->next;
-    while (curr != NULL)
+    while (curr)
     {
         temp = curr;
         prev = curr->prev;
+        curr = curr->next;
 
-        while (prev != NULL && temp->n < prev->n)
+        while (prev && temp->n < prev->n)
         {
-            if (temp->next != NULL)
+            if (temp->next)
                 temp->next->prev = prev;
 
-            prev->next = temp->next;
-            temp->next = prev;
-            temp->prev = prev->prev;
-
-            if (prev->prev != NULL)
+            if (prev->prev)
                 prev->prev->next = temp;
-
-            prev->prev = temp;
-
-            if (temp->prev == NULL)
+            else
                 *list = temp;
 
-            print_list(*list);
-            prev = temp->prev;
-        }
+            temp->prev = prev->prev;
+            prev->next = temp->next;
+            temp->next = prev;
+            prev->prev = temp;
 
-        curr = curr->next;
+            print_list(*list);
+
+            prev = temp->prev;  // Update prev after swapping
+        }
     }
 }
