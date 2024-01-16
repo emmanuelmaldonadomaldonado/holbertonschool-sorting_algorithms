6c04c412b7ac7de68495895b28c0b3ff3837ebf7
millienytorressanta <tmillieny@gmail.com>
2024-01-15 23:24:51 -0800

    algorithms

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,53 +1,46 @@
+#include <stdlib.h>
 #include "sort.h"
-#include <stdlib.h>  /* Include the necessary header file */
 
 /**
- * counting_sort - Sorts an array of integers in ascending order using Counting sort
- * @array: The array to be sorted
- * @size: Size of the array
+ * counting_sort - Sorts an array of integers in ascending order using
+ *                 the Counting sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
-void counting_sort(int *array, size_t size) {
-    if (array == NULL || size <= 1) {
+void counting_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
         return;
-    }
 
-    /* Find the maximum value in the array */
     int max = array[0];
-    size_t i;
+    size_t i, j, index = 0;
+    int *counting_array;
 
-    for (i = 1; i < size; i++) {
-        if (array[i] > max) {
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > max)
             max = array[i];
-        }
     }
 
-    /* Create a counting array with size (max + 1) and initialize to 0 */
-    int *counting_array = malloc((max + 1) * sizeof(int));
-    if (counting_array == NULL) {
-        return; /* Unable to allocate memory */
-    }
+    counting_array = malloc((max + 1) * sizeof(int));
 
-    for (int j = 0; j <= max; j++) {
+    if (counting_array == NULL)
+        return;
+
+    for (j = 0; j <= (size_t)max; j++)
         counting_array[j] = 0;
-    }
 
-    /* Populate the counting array with the occurrences of each element in the input array */
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
         counting_array[array[i]]++;
-    }
 
-    /* Print the counting array */
-    print_array(counting_array, max + 1);
-
-    /* Update the original array using the counting array */
-    size_t j = 0;
-    for (int i = 0; i <= max; i++) {
-        while (counting_array[i] > 0) {
-            array[j++] = i;
+    for (i = 0; i <= (size_t)max; i++)
+    {
+        while (counting_array[i] > 0)
+        {
+            array[index++] = (int)i;
             counting_array[i]--;
         }
     }
 
-    /* Free the counting array */
     free(counting_array);
 }
